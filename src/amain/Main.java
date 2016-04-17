c0c9d7c09e094980a0b47337e72ca78d225a6cdf
Orikuro <vindictus@nexoneu.game>
2016-04-17 23:29:53 +0200

    options

--- src/amain/Main.java
+++ src/amain/Main.java
@@ -1,4 +1,5 @@
 package amain;
+
 import java.util.Comparator;
 
 import org.kohsuke.args4j.CmdLineParser;
@@ -18,13 +19,12 @@ public class Main {
 		crit, hp, def
 	};
 
-	
 	// Main-Infos
-	@Option(name = "-sets", usage = "sets to use use , for more than one. eg. \"Blackram, Mushin\"")
+	@Option(name = "-sets", required = true, usage = "sets to use use , for more than one. eg. \"Blackram, Mushin\"")
 	public static String SETS = "";
-	
+
 	// sort
-	@Option(name = "-sort", usage = "minimum crit the combo must have, default: None")
+	@Option(name = "-sort", required = true, usage = "minimum crit the combo must have, default: None")
 	private void setComparator(Sort sort) {
 		switch (sort) {
 		case crit:
@@ -41,45 +41,44 @@ public class Main {
 			break;
 		}
 	}
-	
+
 	// Buffs
 	// TODO: buffs
-	
+
 	// Enchants
 	@Option(name = "-cenchant", usage = "the number of results, default: 30")
-	public static  int CRIT_ENCHANT = 30;
-	
+	public static int CRIT_ENCHANT = 30;
+
 	// Restrictions
 	@Option(name = "-crit", usage = "minimum crit the combo must have, default: None")
 	public int CRIT = Integer.MIN_VALUE;
 	@Option(name = "-hp", usage = "minimum hp the combo must have, default: None")
-	public static  int HP = Integer.MIN_VALUE;
+	public static int HP = Integer.MIN_VALUE;
 	@Option(name = "-def", usage = "minimum def the combo must have, default: None")
-	public static  int DEF = Integer.MIN_VALUE;
+	public static int DEF = Integer.MIN_VALUE;
 	@Option(name = "-accu", usage = "minimum accu the combo must have, default: None")
-	public static  int ACCU = Integer.MIN_VALUE;
+	public static int ACCU = Integer.MIN_VALUE;
 	@Option(name = "-pierce", usage = "minimum pierce the combo must have, default: None")
-	public static  int PIERCE = Integer.MIN_VALUE;
+	public static int PIERCE = Integer.MIN_VALUE;
 	@Option(name = "-critonly", usage = "dont write .csv files (usefull for gui comparing with open consoles)")
-	public static  boolean CRITONLY = false;
-	
-	
+	public static boolean CRITONLY = false;
+
 	// Output
 	@Option(name = "-results", aliases = { "-r" }, usage = "the number of results, default: 30")
-	public static  int RESULTS = 30;
+	public static int RESULTS = 30;
 	@Option(name = "-force", aliases = { "-f", "-cpus" }, usage = "force number of cpus, default: Systems Maximum")
-	public static  int CPUS = Runtime.getRuntime().availableProcessors();
-	@Option(name = "-noinfo", aliases = { "-no" }, usage = "if set, disables writing of .txt info files, default: false")
-	public static  boolean NOINFO = false;
+	public static int CPUS = Runtime.getRuntime().availableProcessors();
+	@Option(name = "-noinfo", aliases = {
+			"-no" }, usage = "if set, disables writing of .txt info files, default: false")
+	public static boolean NOINFO = false;
 	@Option(name = "-nocsv", usage = "dont write .csv files (usefull for gui comparing with open consoles)")
-	public static  boolean NOCSV = false;
+	public static boolean NOCSV = false;
 
-	
 	public static void main(String[] args) throws Exception {
 		System.out.println(VERSION);
 
 		if (args == null || args.length < 1) {
-			//GUI.main(args);
+			// GUI.main(args);
 		}
 
 		new Main().startMain(args);
@@ -102,7 +101,6 @@ public class Main {
 			return;
 		}
 
-		
 		// Import Shields
 		SoulShields shields = CSVImport.importSoulShields();
 
