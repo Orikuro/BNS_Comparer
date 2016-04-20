5acfe428585aaaeae7a5866ab18b7e75a44ccd04
Orikuro <vindictus@nexoneu.game>
2016-04-20 22:10:15 +0200

    new comparators

---
+++ src/comparators/DEF_SoulShield_Comparator.java
@@ -0,0 +1,22 @@
+package comparators;
+
+import java.util.Comparator;
+
+import modell.SoulShield;
+
+public class DEF_SoulShield_Comparator implements Comparator<SoulShield> {
+
+	@Override
+	public int compare(SoulShield o1, SoulShield o2) {
+		Integer a_crit = o1.getDef();
+		Integer b_crit = o2.getDef();
+
+		if (a_crit == b_crit){
+			a_crit = o1.calcTotalAttributeBonus();
+			b_crit = o2.calcTotalAttributeBonus();
+		}
+
+		return b_crit.compareTo(a_crit);
+	}
+
+}
